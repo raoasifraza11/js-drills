235674b498953bc19aec1d2215ec1a8d0d1bf0e0
raoasifraza11 <raoasifraza1@hotmail.com>
2017-01-22 21:39:07 +0500

    Trafic light drill unsolved

--- app.js
+++ app.js
@@ -281,5 +281,55 @@ console.log(greeting);
 console.log("No Return is -> " + nodefined + ".");
 console.log(avg);
 
+/* Trafic light task */
+function doTrafficLights() {
+    var activeLight = getActiveLight();
+    // your code will replace this call
+    // to `console.log()`
+    console.log(activeLight);
+
+}
+
+
+/* From here down, you are not expected to
+ understand.... for now :)
+
+
+ Nothing to see here!
+
+ */
+
+
+function turnOffLights() {
+    $('.traffic-light').removeClass('yellow-on red-on green-on');
+}
+
+function turnGreen() {
+    turnOffLights();
+    $('.green-light').addClass('green-on');
+}
+
+function turnYellow() {
+    turnOffLights();
+    $('.yellow-light').addClass('yellow-on');
+}
+
+function turnRed() {
+    turnOffLights();
+    $('.red-light').addClass('red-on');
+}
+
+function getActiveLight() {
+    return (['red', 'green', 'yellow'])[Math.floor(Math.random() * 3)];
+}
+
+function handleClicks() {
+    $('.js-control-lights').click(function() {
+        doTrafficLights();
+    });
+}
+
+$(handleClicks);
+
 
 
