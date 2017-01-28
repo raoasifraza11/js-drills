856f23a12e52057c55ed3269ec9eeed9b237ad22
raoasifraza11 <raoasifraza1@hotmail.com>
2017-01-28 13:43:50 +0500

    Object keys friend function

--- app.js
+++ app.js
@@ -1045,3 +1045,17 @@ myFunc2(myVar); // => logs 'bizz'  — that's what we thought would happen!
 console.log(myVar.foo); // => 'bizz'  — ruh roh! change inside function affected outside world!
 
 
+var pageViewCounts = {
+    homePage: 399,
+    aboutPage: 400,
+    termsOfService: 22
+};
+
+console.log(Object.keys(pageViewCounts));
+
+Object.keys(pageViewCounts).forEach(function(key){
+    console.log(
+        'the `' + key + '` page has ' + pageViewCounts[key] +
+        ' views.'
+    );
+})
