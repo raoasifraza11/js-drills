338d6fe6081046eace199a3d17e709244d3bf328
raoasifraza11 <raoasifraza1@hotmail.com>
2017-01-28 13:35:21 +0500

    Pass by refrence

--- app.js
+++ app.js
@@ -1034,3 +1034,14 @@ var sampleObj = {
 })();
 
 
+function myFunc2(obj) {
+    obj.foo = 'bizz';
+    console.log(obj.foo);
+}
+
+var myVar = {foo: 'bar'};
+console.log(myVar.foo); // => 'bar'
+myFunc2(myVar); // => logs 'bizz'  — that's what we thought would happen!
+console.log(myVar.foo); // => 'bizz'  — ruh roh! change inside function affected outside world!
+
+
